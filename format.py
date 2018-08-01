c5d09b65faa5ba2c1f566301037464b674a072dd
alaludan <3131806@qq.com>
2018-08-01 19:00:19 +0800

    Format print

---
+++ format.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+project_id = "23f239942ffe450ea8adc752c8f5350a"
+title = "Disable Project"
+str_ = project_id + " " + "disable succeessful"
+print "\n"
+print title.center(100,'*')
+print "*" + " "*98 + "*"
+print str_.center(100,"*")
+print "*" + " "*98 + "*"
+print "*"*100
