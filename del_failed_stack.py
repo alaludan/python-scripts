c6766767f8b07b86596d44a1cc98dee8b468545e
alaludan <3131806@qq.com>
2018-08-06 00:08:58 -0500

    Clean in use volumes of delete failed stack

--- del_failed_stack.py
+++ del_failed_stack.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 import os
 import time
+import sys
 
 del_failed_stack_list = []
 del_failed_resource_list = []
 del_failed_sub_resource_list = []
 del_failed_volume_list = []
-
 
 def get_stack_id():
     stack_info = os.popen("heat stack-list -g|grep DELETE_FAILED|awk -F'|' '{print $2}'")
@@ -23,7 +23,19 @@ def get_resource_id():
         for resource_id in resource_ids:
             resource_id = resource_id.strip()
             del_failed_resource_list.append(resource_id)
+    print "del_failed_resource_list:%s" % (del_failed_resource_list)
     return del_failed_resource_list
+
+def get_resource_id_from_stack():
+    stack_id = str(sys.argv[1])
+    resource_info = os.popen("heat resource-list %s|grep DELETE_FAILED|awk -F'|' '{print $3}'" % (stack_id))
+    resource_ids = resource_info.readlines()
+    for resource_id in resource_ids:
+        resource_id = resource_id.strip()
+        del_failed_resource_list.append(resource_id)
+    print "del_failed_resource_list:%s" % (del_failed_resource_list)
+    return del_failed_resource_list
+     
     
 def get_sub_resource_id():
     for sub_resource_id in del_failed_resource_list:
@@ -32,6 +44,7 @@ def get_sub_resource_id():
         for sub_resource_id in sub_resource_ids:
             sub_resource_id = sub_resource_id.strip()
             del_failed_sub_resource_list.append(sub_resource_id)
+    print "del_failed_sub_resource_list:%s" % (del_failed_sub_resource_list)
     return del_failed_sub_resource_list
 
 def get_volume_id():
@@ -41,7 +54,8 @@ def get_volume_id():
         for volume_id in volume_ids:
             volume_id = volume_id.strip()
             del_failed_volume_list.append(volume_id)
-        return del_failed_volume_list
+    print "del_failed_volume_list:%s" % (del_failed_volume_list)
+    return del_failed_volume_list
 
 def del_volume():
     for volume_id in del_failed_volume_list:
@@ -54,11 +68,17 @@ def del_stack():
         os.system("heat stack-delete %s -y" % (stack_id))        
 
 def main():
-    get_stack_id()
-    get_resource_id()        
+    get_resource_id_from_stack()        
     get_sub_resource_id()       
     get_volume_id() 
     del_volume()
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 2:
+        print "\n"
+        print ">>>ERROR:lost stack id..."
+        print "\n"
+        print "Usage:%s stack_id" % (sys.argv[0])
+        print "\n"
+    else:
+        main()
